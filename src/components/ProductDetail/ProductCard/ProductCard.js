b75efbeae3445b8f38b6dd34bdff1475143efd0e
Bui Ngo Hoang Long <buingohoanglong2000@gmail.com>
2021-04-06 18:19:17 +0700

    style ProductCard

--- src/components/ProductDetail/ProductCard/ProductCard.js
+++ src/components/ProductDetail/ProductCard/ProductCard.js
@@ -9,16 +9,16 @@ import { addProduct } from "feature/Cart/CartSlice"
 import { useRouteMatch } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 
-// ProductCard.propTypes = {
-//     id: PropTypes.number.isRequired,
-//     title: PropTypes.string.isRequired,
-//     rating: PropTypes.number.isRequired,
-//     numComments: PropTypes.number.isRequired,
-//     numItemsSold: PropTypes.number.isRequired,
-//     price: PropTypes.number.isRequired,
-//     description: PropTypes.string.isRequired,
-//     img: PropTypes.string.isRequired,
-// };
+ProductCard.propTypes = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    numComments: PropTypes.number.isRequired,
+    numItemsSold: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+};
 
 function ProductCard(props) {
     const { title, rating, numComments, numItemsSold, price, description, img } = props
@@ -42,41 +42,41 @@ function ProductCard(props) {
 
     return (
         <div>
-            <Container className='my-5'>
+            <Container className='productcard'>
                 <Row className='justify-content-between'>
                     <Col lg='6'>
                         <Row className='justify-content-between mb-3'>
-                            <Col lg='12' className='px-2 mb-3'><img alt='macbook' className='img-fluid' src={img} /></Col>
-                            <Col lg='4' className='px-2 mb-3'><img alt='macbook' className='img-fluid' src={img} /></Col>
-                            <Col lg='4' className='px-2 mb-3'><img alt='macbook' className='img-fluid' src={img} /></Col>
-                            <Col lg='4' className='px-2 mb-3'><img alt='macbook' className='img-fluid' src={img} /></Col>
+                            <Col lg='12' className='productcard__img'><img alt='macbook' src={img} /></Col>
+                            <Col lg='4' className='productcard__img'><img alt='macbook' src={img} /></Col>
+                            <Col lg='4' className='productcard__img'><img alt='macbook' src={img} /></Col>
+                            <Col lg='4' className='productcard__img'><img alt='macbook' src={img} /></Col>
                         </Row>
                     </Col>
-                    <Col className='d-flex flex-column align-items-start' lg='6'>
+                    <Col className='productcard__info' lg='6'>
                         <h1>{title}</h1>
-                        <div style={{ display: 'flex', justifyContent: 'start' }}>
-                            <div className='mx-2 d-flex'>
+                        <div className='productcard__info__statistic'>
+                            <div className='productcard__info__rating'>
                                 <div>{rating}</div>
                                 {[...Array(5)].map(() => (
                                     <div><AiFillStar color='red' /></div>
                                 ))}
                             </div>
                             <div>|</div>
-                            <div className='mx-2'>{numComments} comments</div>
+                            <div className='productcard__info__comment'>{numComments} comments</div>
                             <div>|</div>
-                            <div className='mx-2'>{numItemsSold} items sold</div>
+                            <div className='productcard__info__itemsold'>{numItemsSold} items sold</div>
                         </div>
-                        <h3 className='price'>{price}$</h3>
-                        <h2 className='description'>Description</h2>
-                        <div className='text-left'>{description}</div>
-                        <div className='d-flex align-items-center'>
-                            <div className='quantity'>Quantity:</div>
+                        <h3 className='productcard__info__price'>{price}$</h3>
+                        <h2 className='productcard__info__description--header'>Description:</h2>
+                        <div className='productcard__info__description--text'>{description}</div>
+                        <div className='productcard__info__quantity'>
+                            <div>Quantity:</div>
                             <QuantityAdder quantity={quantity} maxItems={5} onIncrease={handleOnIncrease} onDecrease={handleOnDecrease} />
                         </div>
-                        <Button color="primary"
+                        <Button className='productcard__info__addtocart' color='primary'
                             onClick={handleOnAddToCart}
                         >
-                            Add to cart
+                            ADD TO CART
                         </Button>
                     </Col>
                 </Row>
